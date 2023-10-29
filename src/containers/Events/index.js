c36ac191b1beb32d4019f78daa11a9b4bb1c8d00
Arthur Le Meur <arthur@edrlab.org>
2023-10-29 11:30:30 +0100

    project completed

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import EventCard from "../../components/EventCard";
 import Select from "../../components/Select";
 import { useData } from "../../contexts/DataContext";
@@ -10,67 +11,83 @@ import "./style.css";
 const PER_PAGE = 9;
 
 const EventList = () => {
-  const { data, error } = useData();
-  const [type, setType] = useState();
-  const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
-  const changeType = (evtType) => {
-    setCurrentPage(1);
-    setType(evtType);
-  };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  const typeList = new Set(data?.events.map((event) => event.type));
-  return (
-    <>
-      {error && <div>An error occured</div>}
-      {data === null ? (
-        "loading"
-      ) : (
+    const { data, error } = useData();
+    const [type, setType] = useState();
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const filteredArray = data?.events.filter((event) => {
+        if (!type || event.type === type) {
+            return true;
+        }
+        return false;
+    });
+
+    const filteredEvents = (
+        (!type ? data?.events : filteredArray) || []
+    ).filter((event, index) => {
+        if (
+            (currentPage - 1) * PER_PAGE <= index &&
+            PER_PAGE * currentPage > index
+        ) {
+            return true;
+        }
+        return false;
+    });
+
+    // const changeType = (evtType) => {
+    //     setCurrentPage(1);
+    //     setType(evtType);
+    // };
+    const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+    const typeList = new Set(data?.events.map((event) => event.type));
+    return (
         <>
-          <h3 className="SelectTitle">Catégories</h3>
-          <Select
-            selection={Array.from(typeList)}
-            onChange={(value) => (value ? changeType(value) : changeType(null))}
-          />
-          <div id="events" className="ListContainer">
-            {filteredEvents.map((event) => (
-              <Modal key={event.id} Content={<ModalEvent event={event} />}>
-                {({ setIsOpened }) => (
-                  <EventCard
-                    onClick={() => setIsOpened(true)}
-                    imageSrc={event.cover}
-                    title={event.title}
-                    date={new Date(event.date)}
-                    label={event.type}
-                  />
-                )}
-              </Modal>
-            ))}
-          </div>
-          <div className="Pagination">
-            {[...Array(pageNumber || 0)].map((_, n) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
-                {n + 1}
-              </a>
-            ))}
-          </div>
+            {error && <div>An error occured</div>}
+            {data === null ? (
+                "loading"
+            ) : (
+                <>
+                    <h3 className="SelectTitle">Catégories</h3>
+                    <Select
+                        selection={Array.from(typeList)}
+                        setType={setType}
+                        onChange={() => setCurrentPage(1)}
+                    />
+                    <div id="events" className="ListContainer">
+                        {filteredEvents.map((event) => (
+                            <Modal
+                                key={uuidv4()}
+                                Content={<ModalEvent event={event} />}
+                            >
+                                {({ setIsOpened }) => (
+                                    <EventCard
+                                        onClick={() => setIsOpened(true)}
+                                        imageSrc={event.cover}
+                                        title={event.title}
+                                        date={new Date(event.date)}
+                                        label={event.type}
+                                        key={uuidv4()}
+                                    />
+                                )}
+                            </Modal>
+                        ))}
+                    </div>
+                    <div className="Pagination">
+                        {[...Array(pageNumber || 0)].map((_, n) => (
+                            // eslint-disable-next-line react/no-array-index-key
+                            <a
+                                key={uuidv4()}
+                                href="#events"
+                                onClick={() => setCurrentPage(n + 1)}
+                            >
+                                {n + 1}
+                            </a>
+                        ))}
+                    </div>
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 };
 
 export default EventList;
--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,59 +1,85 @@
 import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
 
 const Slider = () => {
-  const { data } = useData();
-  const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
+    const { data } = useData();
+    const [index, setIndex] = useState(0);
+    const byDateDesc = data?.focus.sort((evtA, evtB) =>
+        new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
     );
-  };
-  useEffect(() => {
-    nextCard();
-  });
-  return (
-    <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+
+    let timer;
+
+    const timeout = () => {
+        timer = setTimeout(() => {
+            setIndex((prevIndex) =>
+                prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0
+            );
+        }, 5000);
+    };
+
+    const nextCard = () => {
+        if (data?.focus && data.focus.length > 1) {
+            timeout();
+        }
+    };
+
+    const pagination = (radioIdx) => {
+        clearTimeout(timer);
+        setIndex(radioIdx);
+        timeout();
+    };
+
+    useEffect(() => {
+        nextCard();
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [index, byDateDesc, data]);
+
+    if (!byDateDesc || byDateDesc.length === 0) {
+        return null;
+    }
+
+    return (
+        <div className="SlideCardList">
+            {byDateDesc?.map((event, idx) => (
+                <div key={uuidv4()}>
+                    <div
+                        className={`SlideCard SlideCard--${
+                            index === idx ? "display" : "hide"
+                        }`}
+                    >
+                        <img src={event.cover} alt="forum" />
+                        <div className="SlideCard__descriptionContainer">
+                            <div className="SlideCard__description">
+                                <h3>{event.title}</h3>
+                                <p>{event.description}</p>
+                                <div>{getMonth(new Date(event.date))}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            <div className="SlideCard__paginationContainer" key={uuidv4()}>
+                <div className="SlideCard__pagination">
+                    {byDateDesc.map((_, radioIdx) => (
+                        <input
+                            key={uuidv4()}
+                            type="radio"
+                            name="radio-button"
+                            defaultChecked={index === radioIdx}
+                            onClick={() => pagination(radioIdx)}
+                        />
+                    ))}
+                </div>
             </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
-      ))}
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Slider;
